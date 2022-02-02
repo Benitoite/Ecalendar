f342416a5c5c800e720184079473c659dd11bec9
Benitoite <kd6kxr@gmail.com>
2022-02-02 03:46:27 -0800

    Update ecal.py

--- ecal.py
+++ ecal.py
@@ -19,7 +19,7 @@ with open('static/el1.html', 'w') as f:
       <title>E Calendar</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="36" />
+    <meta http-equiv="refresh" content="3600" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
 
