e05bae1595cefb4fea5415bd41884956711c5cba
Benitoite <kd6kxr@gmail.com>
2022-02-03 20:26:04 -0800

    Update ecal.py

--- ecal.py
+++ ecal.py
@@ -56,8 +56,8 @@ with open('static/el1.html', 'w') as f:
     0 1px 1px black, 
     0 2px 2px cyan;">''')
     
-    print('<div id="wrap" style="position:relative; top: 0px; right: 0px;"><iframe id="scaled-frame"   frameBorder="0" src="https://17230.ddns.net/static/pac.html"></iframe></div>')
     print('<h1>Today is '+str(today.isoformat()[:10]))
+    print('<div id="wrap" style="position:relative; top: 0px; left: 0px;"><iframe id="scaled-frame"   frameBorder="0" src="https://17230.ddns.net/static/pac.html"></iframe></div>')
     
     print('<center><h1 style="text-shadow:0 0 1px black,0 5px 10px black,0 10px 20px cyan;font-family:Helvetica;font-size:255px;">')
     
